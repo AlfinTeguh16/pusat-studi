17f003595c7c961426d9f0b2e7a7eb3b202acb68
jikdez <ajikdezz@gmail.com>
2024-02-29 21:16:46 +0800

    galery

--- resources/views/home/pusatstudi.blade.php
+++ resources/views/home/pusatstudi.blade.php
@@ -135,40 +135,40 @@
 </div>
 <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
     <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="">
+        <img class="h-auto max-w-full rounded-lg" src="asset/images/galery1.jpg" alt="">
     </div>
     <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="">
+        <img class="h-auto max-w-full rounded-lg" src="asset/images/galery3.jpg" alt="">
     </div>
     <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="">
+        <img class="h-auto max-w-full rounded-lg" src="asset/images/galery2.jpg" alt="">
     </div>
     <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
+        <img class="h-auto max-w-full rounded-lg" src="asset/images/galery4.jpg" alt="">
     </div>
     <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="">
+        <img class="h-auto max-w-full rounded-lg" src="asset/images/galery5.jpg" alt="">
     </div>
     <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt="">
+        <img class="h-auto max-w-full rounded-lg" src="asset/images/galery6.jpg" alt="">
     </div>
     <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg" alt="">
+        <img class="h-auto max-w-full rounded-lg" src="asset/images/galery7.jpg" alt="">
     </div>
     <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg" alt="">
+        <img class="h-auto max-w-full rounded-lg" src="asset/images/galery8.jpg" alt="">
     </div>
     <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg" alt="">
+        <img class="h-auto max-w-full rounded-lg" src="asset/images/galery9.jpg" alt="">
     </div>
     <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg" alt="">
+        <img class="h-auto max-w-full rounded-lg" src="asset/images/galery10.jpg" alt="">
     </div>
     <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg" alt="">
+        <img class="h-auto max-w-full rounded-lg" src="asset/images/galery12.jpg" alt="">
     </div>
     <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg" alt="">
+        <img class="h-auto max-w-full rounded-lg" src="asset/images/galery11.jpg" alt="">
     </div>
 </div>
 
